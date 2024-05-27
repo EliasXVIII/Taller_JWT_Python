bd206b15d5b12689651948e982bf5d34c4d08d8c
EliasJavierRiquelme <elias.renault4@gmail.com>
2024-05-27 22:14:57 +0200

    JWT Iniciado

---
+++ main.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI, HTTPException
+import time
+from jose import jwt
+from fastapi.staticfiles import StaticFiles
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+clave_secreta = getenv("clave_secreta")
+
+app = FastAPI()
+
+caducidad = int(time.time()) + 240
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+token = jwt.encode({"id":"1", "nombre": "Elias", "time": caducidad}, key=clave_secreta, algorithm="HS256")
+print(token)
+
+print("----------------------")
+
+token_resuelto = jwt.decode(token, key=clave_secreta, algorithms=["HS256"])
+print(token_resuelto)
+
+@app.get("/token")
+def get_token():
+    return {"token": token}
+
+def verificar_token(token):
+    payload = jwt.decode(token, clave_secreta, algorithms=["HS256"])
+    return payload
+
+@app.post("/verificar/{token}")
+def verificar_token_route(token: str):
+    payload = verificar_token(token)
+    return payload
+    
